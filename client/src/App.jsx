d68e2324f3554b7607b4f7e2ab633027e27cc822
Faizan Naeem <faizannaeem7799@gmail.com>
2026-08-13 20:45:47 +0500

    Created the routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,12 +1,21 @@
+/* eslint-disable no-undef */
+/* eslint-disable no-unused-vars */
 
 import './App.css'
+import { Route,Routes } from 'react-router-dom'
 
 function App() {
 
 
   return (
     <>
-     
+     <Routes>
+      <Route path='/' element = {<Home/>}/>
+      <Route path='/profile' element = {<Profile/>}/>
+      <Route path='/about' element = {<About/>}/>
+      <Route path='/sign-in' element = {<SignIn/>}/>
+      <Route path='/sign-out' element = {<SignOut/>}/>
+      </Routes>
     </>
   )
 }
