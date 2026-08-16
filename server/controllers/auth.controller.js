01019c0fa2383b0df968a960ab8e9a5ce59ad0a3
Faizan Naeem <faizannaeem7799@gmail.com>
2026-08-17 00:30:05 +0500

    Completed the SignIn route

--- server/controllers/auth.controller.js
+++ server/controllers/auth.controller.js
@@ -1,6 +1,8 @@
 import User from "../models/user.model.js"
 import bcrypt from "bcrypt"
 import { errorhandler } from "../utils/error.js"
+import jwt from "jsonwebtoken"
+
 
 
 export const signup = async (req,res,next)=>
@@ -15,4 +17,35 @@ export const signup = async (req,res,next)=>
         next(error)
      }
       
+}
+
+export const signin = async (req,res,next)=>
+{
+    const {email, password} = req.body;
+    try {
+        const ValidUser = await User.findOne({
+            email
+        })
+
+        if(!ValidUser)
+        {
+           return next(errorhandler(404,"User not Found"))
+        }
+        const validPassword = bcrypt.compareSync(password,ValidUser.password)
+        if(!validPassword){
+            return next(errorhandler(401,"Invalid Credentials"))
+        }
+
+        const token = jwt.sign({
+            id: ValidUser._id
+        }, process.env.JWT_SECRET)
+
+        const {password:pass, ...rest} = ValidUser._doc
+
+         res.cookie('access_token', token,{httpOnly: true,
+         }).status(200).json(ValidUser)
+
+    } catch (error) {
+        next(error)
+    }
 }
