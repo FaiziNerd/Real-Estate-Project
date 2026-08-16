e1e7c15a230637b731d17d5a1d76ecae9b94d4f8
Faizan Naeem <faizannaeem7799@gmail.com>
2026-08-16 19:08:33 +0500

    Fixed bugs related to the sign-up.Also the typo

--- client/src/pages/SignUp.jsx
+++ client/src/pages/SignUp.jsx
@@ -1,22 +1,82 @@
-  import { Link } from "react-router-dom"
+/* eslint-disable no-unused-vars */
+  import { useState } from "react"
+import { Link, useNavigate } from "react-router-dom"
 
 
   function SignUp()
+ {
+
+  const[error,setError] = useState(null)
+  const[loading,setloading] = useState(false)
+  const navigate = useNavigate()
+
+  const[formData,setFormData] = useState({
+    username: "",
+    email: "",
+    password:""
+
+  })
+
+  function handleChange(e)
   {
+     setFormData({
+      ...formData,
+      [e.target.id] : e.target.value
+     })
+  }
+
+  async function handlesubmit(e)
+  {
+     e.preventDefault()
+
+     try {
+      setloading(true)
+     const res = await fetch('/api/auth/sign-up',
+      {
+        method: 'POST',
+        headers:
+        {
+          'Content-Type' : 'application/json'
+
+        },
+        body: JSON.stringify(formData)
+      },
+     
+     )
+     const data = await res.json()
+     if(data.success === false)
+     {
+      setError(data.message)
+      setloading(false)
+      return;
+     }
+     setloading(false)
+     setError(null)
+     navigate('/sign-in')
+    }
+      catch (error) {
+      setloading(false)   
+      setError(error.message)
+     }
+    
+  }
+
+
   return (
     <div className="p-3 max-w-lg mx-auto">
       <h1 className="text-3xl text-center font-semibold
       my-7">Sign Up</h1>
 
-      <form className="flex flex-col gap-2">
-        <input type="text" placeholder="username" className="border p-3 rounded-lg" id="username" />
+      <form onSubmit={handlesubmit} className="flex flex-col gap-2">
+        <input  onChange={handleChange} type="text" placeholder="username" className="border p-3 rounded-lg" id="username" />
 
-        <input type="email" placeholder="email" className="border p-3 rounded-lg" id="email" />
+        <input  onChange={handleChange} type="email" placeholder="email" className="border p-3 rounded-lg" id="email" />
 
-        <input type="password" placeholder="password" className="border p-3 rounded-lg" id="password" />
+        <input onChange={handleChange} type="password" placeholder="password" className="border p-3 rounded-lg" id="password" />
 
-        <button className="bg-slate-700 text-white p-3
-        rounded-lg uppercase hover:opacity-95 disabled:opacity-80">Sign up</button>
+        <button  disabled={loading} className="bg-slate-700 text-white p-3
+        rounded-lg uppercase hover:opacity-95 disabled:opacity-80">
+          {loading ? "Loading!!.." : "SignUp"}</button>
 
 
       </form>
@@ -29,6 +89,7 @@
         <span className="text-blue-800">Sign in</span>
         </Link>
       </div>
+      {error && <p className="text-red-600 mt-5">{error}</p>  }
     </div>
   )
   }
--- server/controllers/auth.controller.js
+++ server/controllers/auth.controller.js
@@ -3,7 +3,7 @@ import bcrypt from "bcrypt"
 import { errorhandler } from "../utils/error.js"
 
 
-export const signup = async (req,res)=>
+export const signup = async (req,res,next)=>
 {
     const {username, email, password} = req.body
     const hashedPassword = bcrypt.hashSync(password,10)
