f9afe83df461835863fb6e9cf7db149c3a928e12
Faizan Naeem <faizannaeem7799@gmail.com>
2026-08-27 23:57:24 +0500

    Added the get api route for listings

--- server/controllers/listing.controller.js
+++ server/controllers/listing.controller.js
@@ -1,4 +1,5 @@
 import Listing from "../models/listing.model.js"
+import { errorhandler } from "../utils/error.js"
 
 
 
@@ -11,4 +12,23 @@ export const createListing = async (req,res,next)=>
     } catch (error) {
         next(error)
     }
+}
+
+
+export const getUserListings = async (req,res,next)=>
+{
+   if(req.user.id === req.params.id)
+   {
+    try {
+        const listing = await Listing.find({userRef: req.params.id})
+        res.status(200).json(listing)
+    } catch (error) {
+        next(error)
+    }
+   }
+
+   else 
+   {
+    return next(errorhandler(401, "You can create your listings!!"))
+   }
 }
--- server/routes/listing.route.js
+++ server/routes/listing.route.js
@@ -2,9 +2,12 @@ import { verifyToken } from "../utils/verifyUser.js"
 import express from "express"
 import { createListing } from "../controllers/listing.controller.js"
 
+import { getUserListings } from "../controllers/listing.controller.js"
+
 const router = express.Router()
 
 router.post('/create',verifyToken,createListing)
+router.get('/listings/:id',verifyToken, getUserListings)
 
 
 
