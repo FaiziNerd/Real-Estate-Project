5e8eb7a10397e99ceb206d225d3854c4becebc57
Faizan Naeem <faizannaeem7799@gmail.com>
2026-09-02 11:58:30 +0500

    feat(feature): Added the Search API route

--- server/routes/listing.route.js
+++ server/routes/listing.route.js
@@ -3,7 +3,7 @@ import express from "express"
 import { createListing } from "../controllers/listing.controller.js"
 import { deleteuserListings } from "../controllers/listing.controller.js"
 import { updateuserListings } from "../controllers/listing.controller.js"
-import {getListing} from "../controllers/listing.controller.js"
+import {getListing, getAllListings} from "../controllers/listing.controller.js"
 
 
 const router = express.Router()
@@ -12,6 +12,7 @@ router.post('/create',verifyToken,createListing)
 router.delete('/delete/:id',verifyToken,deleteuserListings)
 router.post('/update/:id',verifyToken,updateuserListings)
 router.get('/get/:id',getListing)
+router.get('get', getAllListings)
 
 
 
