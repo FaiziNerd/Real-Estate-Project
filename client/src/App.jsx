c27ff07f3b71d44f48d74fdb674c7e6b6c64a8ba
Faizan Naeem <faizannaeem7799@gmail.com>
2026-09-02 18:20:26 +0500

    feat(feature): Completed the UI page for the Search

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -14,6 +14,7 @@ import PrivateRoute from './components/PrivateRoute'
 import CreateListing from './pages/CreateListing'
 import UpdateListing from './pages/UpdateListing'
 import Listing from './pages/Listing'
+import Search from './pages/Search'
 
 
 
@@ -34,7 +35,9 @@ function App() {
       <Route path='/sign-in' element = {<SignIn/>}/>
       <Route path='/sign-up' element = {<SignUp/>}/>
       <Route path='/sign-out' element = {<SignOut/>}/>
-        <Route path='/listing/:listingId' element = {<Listing/>}/>
+      <Route path='/listing/:listingId' element = {<Listing/>}/>
+      <Route path='/search' element = {<Search/>}/>
+
 
       </Routes>
     </>
